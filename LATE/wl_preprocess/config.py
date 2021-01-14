7996b30ebb68ab862a3d9ccefe9100f089f55023
Kyle Sheppard <kshep23@umd.edu>
2021-01-14 16:56:07 -0500

    Changes to make config file usable

---
+++ LATE/wl_preprocess/config.py
@@ -0,0 +1,21 @@
+[DATA]
+planet = l9859b
+visit_number = visit00
+scan_direction = reverse
+transit = yes
+check = yes
+inp_file = no
+data_plots = yes
+
+[MODEL]
+mcmc = no
+openinc = no
+openar = yes
+fixtime = no
+norandomt = yes
+fit_plots = yes
+
+[SAVE]
+save_processed_data = no
+save_model_info = no
+save_mcmc = no
