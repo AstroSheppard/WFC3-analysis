4c5c04c4ba83c111a626ecf2862b8f26b407e926
Kyle Sheppard <kshep23@umd.edu>
2021-01-22 13:11:53 -0500

    config.py file for easier input setting for wl_preprocess analysis

--- LATE/wl_preprocess/config.py
+++ LATE/wl_preprocess/config.py
@@ -1,18 +1,26 @@
 [DATA]
 planet = l9859b
 visit_number = visit00
-scan_direction = reverse
+scan_direction = both
 transit = yes
 check = yes
+ignore_first_exposures = yes
 inp_file = no
 data_plots = yes
+
 
 [MODEL]
 mcmc = no
 openinc = no
-openar = yes
+openar = no
 fixtime = no
 norandomt = yes
+linear_slope = yes
+quad_slope = no
+exp_slope = no
+log_slope = no
+# For bi-directional scan, fit with one slope for both directions.
+one_slope = yes
 fit_plots = yes
 
 [SAVE]
