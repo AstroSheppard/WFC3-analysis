9263eb4bbdbb2914318523e966bea0ae0ba19887
Kyle Sheppard <kshep23@umd.edu>
2021-02-11 03:30:20 -0500

    Added config.py file for data reduction routines.

---
+++ LATE/data_reduction/config.py
@@ -0,0 +1,23 @@
+[DATA]
+planet = l9859b
+visit_number = visit02
+scan_direction = reverse
+transit = yes
+data_plots = yes
+# Set test_bkg to yes to save fits files to see if
+# background window size is appropriate.
+test_bkg = no
+# Typically, positive or negative position in header indicates scan
+# direction. Sometimes this is slightly not true, and an adjustment
+# is necessary to properly classify scan directions. 
+scan_adjustment = 0.0
+
+[COSMIC_RAY]
+first_sigma_cut = 8
+second_sigma_cut = 5
+
+[IMAGE_VIEWER]
+# Image location: bkg (background removal only)
+# or final (complete reduction)
+reduction_level = bkg
+exposure_number = 045
