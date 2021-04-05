6af6a4066171ddd71d9435591cee518564a754bc
Kyle Sheppard <kshep23@umd.edu>
2021-04-05 18:12:00 -0400

    Better labeling

--- LATE/wl_preprocess/config.py
+++ LATE/wl_preprocess/config.py
@@ -1,22 +1,23 @@
-# Inputs to all wl_preprocess files, including: preprocess_whitelight.py,
-# wlresids_new.py, wave_solution.py.
+# Inputs to all wl_preprocess files, including:
+# preprocess_whitelight.py, wlresids_new.py,
+# wave_solution.py.
 
 [DATA]
-planet = l9859c
-visit_number = visit00
+planet = l9859b
+visit_number = visit02
 scan_direction = both
 # Is this a transit, or an eclipse?
 transit = yes
 # Ignore the first few exposures of each orbit to make
 # polynomial approximation more accurate?
-ignore_first_exposures = no
+ignore_first_exposures = yes
 # Only set inp_file to on if using pre-selected inputs.
 # As a default, use "check" instead, which allows user to
 # select orbits to include in analysis.
 check = yes
 inp_file = no
 # Show the processed data pre-fitting?
-data_plots = no
+data_plots = yes
 # For wave_solution.py: show result of wavelength solution fit?
 wave_solution_plots = yes
 
@@ -24,8 +25,8 @@ wave_solution_plots = yes
 # Run MCMC on the best fitting model from the marginalization grid?
 mcmc = no
 
-# Inflate flux errors such that reduced chi-squared = 1 (and so inflating
-# the whitelight depth error)?
+# Inflate flux errors such that reduced chi-squared = 1
+# (and so inflating the whitelight depth error)?
 include_error_inflation = yes
 
 # Physical parameters for which to fit.
@@ -35,25 +36,26 @@ fixtime = no
 # Do not change: norandomt always on.
 norandomt = yes
 
-# Visit long-slopes paramterizations. Default is linear, only use
-# others if testing impact.
+# Visit long-slopes paramterizations. Default is linear.
 linear_slope = yes
-# Do not use higher slopes
-quad_slope = no
+quad_slope = yes
+# Do not use these non-linear slopes yet.
 exp_slope = no
 log_slope = no
 
-# For bi-directional scan, fit with one slope for both directions. Always
-# keep on. 
+# For bi-directional scan, fit with one slope for both
+# directions. Always keep on. 
 one_slope = yes
 
 # Set fit_plots to yes to visualize best fit light curve for
 # each systematic model. Program is much faster when set to
-# no.
+# no. Plot_best only plots the fit and autocorrelation function
+# of the best fitting systematic model.
 fit_plots = no
+plot_best = yes
 
-# Determine what source to use for limb darkening coefficients. Claret2012 is
-# preferred over claret2011 for cooler planets.
+# Determine what source to use for limb darkening coefficients.
+# Claret2012 is preferred over claret2011 for cooler planets.
 limb_source = claret2012.csv
 
 # Fixed nonlinear limb darkening, or fittable linear limb darkening?
@@ -69,4 +71,4 @@ save_processed_data = yes
 save_model_info = yes
 
 # Save outputs of MCMC fit.
-save_mcmc = yes
+save_mcmc = no
